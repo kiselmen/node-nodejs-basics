336297a1eec4c8d5aa1e78ff3fd71246b3f45252
kiselmen <kiselmen@rambler.ru>
2022-12-03 19:31:59 +0300

    feat: add fs operations

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,22 @@
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { rm, stat } from 'node:fs/promises';
+
+const fileName = fileURLToPath(import.meta.url);
+const dirName = dirname(fileName);
+const fileToRemove = join(dirName, 'files', 'fileToRemove.txt');
+
+const isExistItem = async (path, item) => {
+    return item === 'file' ? 
+        await stat(path).then(itemType => itemType.isFile()).catch(error => error.code !== 'ENOENT') : 
+        await stat(path).then(itemType => itemType.isDirectory()).catch(error => error.code !== 'ENOENT')
+}
+
 const remove = async () => {
-    // Write your code here 
+	const isExist = await isExistItem(fileToRemove, 'file');
+	if (!isExist) throw new Error('FS operation failed');
+
+	await rm(fileToRemove);
 };
 
 await remove();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,23 @@
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { readFile, stat } from 'node:fs/promises';
+
+const fileName = fileURLToPath(import.meta.url);
+const dirName = dirname(fileName);
+const sourceFile = join(dirName, 'files', 'fileToRead.txt');
+
+const isExistItem = async (path, item) => {
+    return item === 'file' ? 
+        await stat(path).then(itemType => itemType.isFile()).catch(error => error.code !== 'ENOENT') : 
+        await stat(path).then(itemType => itemType.isDirectory()).catch(error => error.code !== 'ENOENT')
+}
+
 const read = async () => {
-    // Write your code here 
+	const isExist = await isExistItem(sourceFile, 'file');
+
+	if (!isExist) throw new Error('FS operation failed');
+
+	console.log(await readFile(sourceFile, 'utf-8'));
 };
 
 await read();
