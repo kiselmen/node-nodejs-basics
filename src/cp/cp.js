4cf74cfdfad8b8050b9a7480ed36290c42e520da
kiselmen <kiselmen@rambler.ru>
2022-12-05 19:42:37 +0300

    feat: zip, wt, cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,19 @@
+import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { spawn } from 'node:child_process';
+
+const fileName = fileURLToPath(import.meta.url);
+const dirnNme = dirname(fileName);
+const scriptSource = join(dirnNme, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = spawn('node', [scriptSource, ...args]);
+
+    process.stdin.pipe(child.stdin);
+
+    child.stdout.on('data', (data)=>{
+        console.log(data.toString());
+    })
 };
 
-spawnChildProcess();
+spawnChildProcess(['A1', 'A2', 'A3']);
